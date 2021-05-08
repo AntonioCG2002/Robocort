f15b662b33d20377e077d7bedd4f9ba9d80cdb1f
Antonio Cortés <78567128+AntonioCG2002@users.noreply.github.com>
2021-05-08 09:04:32 -0500

    Trabajo realizado en clase 8 de mayo 2021

---
+++ C/Datosentradaysalida(nombre,edad).c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(){
+  int edad;
+  char nombre[10];//Numero de años 
+  printf("Inserte su edad: ");
+  scanf("%d",&edad);
+  printf("Inserte su nombre: ");
+  scanf("%s",&nombre);
+  printf("Edad: %d \t",edad);
+  printf("Nombre: %s",nombre);
+  return 0;
+}
