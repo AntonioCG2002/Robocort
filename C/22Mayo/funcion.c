56843c9bfbe4a2e0a641bb90363d1a90bbf8a0a5
Antonio Cortés <78567128+AntonioCG2002@users.noreply.github.com>
2021-05-22 09:11:17 -0500

    Create funcion.c

---
+++ C/22Mayo/funcion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+void areatriangulo(){
+   printf("Area triangulo\n");
+   float area, base, altura;
+   printf("Escribe la base\n");
+   scanf("%f",&base);
+   printf("Escribe la altura\n");
+   scanf("%f",&altura);
+   area=(base*altura/2);
+   printf("El area es:");
+   printf("%f \n",area);
+}
+
+void areacuadrado(){
+   printf("Area Cuadardo\n");
+   float area, base;
+   printf("Escribe la base\n");
+   scanf("%f",&base);
+   area=base*base;
+   printf("El area es:");
+   printf("%f \n",area);
+}
